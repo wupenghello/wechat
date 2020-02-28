262d00e02a5bc41a5c91fb2ced3b7b2f559e17c9
wupenghello <wu_peng@188.com>
2020-02-28 20:59:36 +0800

    配置微信公众号时，响应微信服务器的配置

--- router.js
+++ router.js
@@ -2,7 +2,7 @@
 * @Author: WuPeng
 * @Date:   2020-02-27 22:09:16
 * @Last Modified by:   WuPeng
-* @Last Modified time: 2020-02-27 22:13:58
+* @Last Modified time: 2020-02-28 20:58:36
 * @Descript:路由
 */
 
@@ -16,11 +16,13 @@ const router = express.Router();
 
 // 定义路由
 router.get('/',(req,res) => {
-	res.send('This is home page .');
+	// res.send('This is home page .');
+	// 
+	// 暂时注释掉 send , 客户端发出一次请求，服务器给出两次及以上响应,node 会有报错信息
 });
 
 router.get('/test', (req,res) => {
-	res.send('This is test page');
+	// res.send('This is test page');
 });
 
 // 导出路由
